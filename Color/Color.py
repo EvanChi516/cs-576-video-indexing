d228d16fd3547340bf198e932a88f3f8bd700a9d
EvanChi516 <ejc.chiang@gmail.com>
2024-04-27 02:38:49 -0700

    Capable of writing down a single frame

--- Color/Color.py
+++ Color/Color.py
@@ -1,27 +1,26 @@
 import cv2
 import os
 
-video_folder = "..\\Videos"      #Replace with path to videos folder 
-video = "..\\Videos\\video1.mp4"
-
+video_folder = "..\\Videos"      # Replace with path to videos folder 
+video = "..\\Videos\\video1.mp4" # A single video for testing purposes
 
 def disassemble_video(video):
-    title = video.str
+    title = video
     vidcap = cv2.VideoCapture(video)
     success, image = vidcap.read()
     count = 0
-    success = True
     while success:
-        success, image = vidcap.read()
-        print('Read a new frame: ', success)
-        # save frame as JPEG file
-        cv2.imwrite("/path/to/video_extract/frame&%d&%s.jpg" % count, title, image)    
+        # save frame as JPG file
+        cv2.imwrite(video_folder + "\\frames\\%s&%d.jpg" % (title, count), image)
         count += 1
+        success, image = vidcap.read()
+        # if(count % 1000 == 0):
+        #     print('Read in frame', count , success)
 
 def load_videos(video_folder):
     images = []
     names = []
-    for filename in os.listdir(video_folder):
+    for filename in os.listdir(video_folder+"\\frames"):
         img = cv2.imread(os.path.join(video_folder, filename))
         name = str(filename)
         if img is not None:
@@ -30,7 +29,14 @@ def load_videos(video_folder):
     return images, names
 
 def main():
-    print("Hello World!")
+    disassemble_video(video)
+
+    vidcap = cv2.VideoCapture(video)
+    success, image = vidcap.read()
+    cv2.imwrite(video_folder + "\\frames\\test.jpg", image)
+    
+    # frames = load_videos(video_folder)
+    # print(frames[1])
 
 if __name__ == "__main__":
     main()
